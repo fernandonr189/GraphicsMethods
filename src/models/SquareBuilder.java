b867104326ff47722f9e04beaf2aff6ce9bf18f3
Fernando Ismael Navarrete Ruiz <fernandonr189@outlook.com>
2024-05-12 01:18:26 -0600

    Fixed rotation positioning

--- src/models/SquareBuilder.java
+++ src/models/SquareBuilder.java
@@ -74,11 +74,10 @@ public class SquareBuilder implements BuildMethods{
             (double) buffer.getHeight() / 2
         };
 
-        double highestX = buffer.getWidth();
-        double highestY = buffer.getHeight();
-
-        double lowestY = 0;
-        double lowestX = 0;
+        double lowestX = buffer.getWidth();
+        double lowestY = buffer.getHeight();
+        double highestX = 0;
+        double highestY = 0;
 
         for(int i = 0; i < points.length; i++) {
             double dx = points[i].getX() - center[0];
@@ -88,43 +87,32 @@ public class SquareBuilder implements BuildMethods{
             double newAngle = originalAngle - angle;
             double newX = b * cos(newAngle) + center[0];
             double newY = b * sin(newAngle) + center[1];
+            points[i] = new Point(newX, newY);
 
-            if(newX > highestX) {
-                highestX = newX;
+            if(points[i].getX() > highestX) {
+                highestX = points[i].getX();
             }
-
-            if(newY > highestY) {
-                highestY = newY;
+            if(points[i].getY() > highestY) {
+                highestY = points[i].getY();
             }
-
-            if(newX < lowestX) {
-                lowestX = newX;
+            if(points[i].getX() < lowestX) {
+                lowestX = points[i].getX();
             }
-
-            if(newY < lowestY) {
-                lowestY = newY;
+            if(points[i].getY() < lowestY) {
+                lowestY = points[i].getY();
             }
+        }
 
+        for(int i = 0; i < points.length; i++) {
+            double newX = points[i].getX() - lowestX + 20;
+            double newY = points[i].getY() - lowestY + 20;
             points[i] = new Point(newX, newY);
         }
 
-        if(lowestX < 0) {
-            for(int i = 0; i < points.length; i++) {
-                points[i] = new Point(points[i].getX() + ceil(abs(lowestX)) + 20, points[i].getY());
-            }
-        }
-        if(lowestY < 0) {
-            for(int i = 0; i < points.length; i++) {
-                points[i] = new Point(points[i].getX(), points[i].getY() + ceil(abs(lowestY)) + 20);
-            }
-        }
-
-        int bufferWidth = (int) floor(highestX) - (int) floor(lowestX) + 40;
-        int bufferHeight = (int) floor(highestY) - (int) floor(lowestY) + 40;
+        int bufferWidth = (int) floor(highestX - lowestX) + 40;
+        int bufferHeight = (int) floor(highestY - lowestY) + 40;
 
         buffer = new CustomBuffer(bufferWidth, bufferHeight, buffer.getType(), this);
-
-
 
         build(buffer);
         return buffer;
